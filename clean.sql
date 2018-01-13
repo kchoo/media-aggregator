ebf3ae8c8eee7ae05b7367170642e82c5b86d3b8
kchoo <35017578+kchoo@users.noreply.github.com>
2018-01-12 22:37:30 -0600

    Update clean.sql

--- clean.sql
+++ clean.sql
@@ -1,3 +1,15 @@
+DROP TRIGGER IF EXISTS log_event ON sites;
+DROP TRIGGER IF EXISTS log_event ON sources;
+DROP TRIGGER IF EXISTS log_event ON images;
+DROP TRIGGER IF EXISTS log_event ON users;
+DROP TRIGGER IF EXISTS log_event ON votes;
+DROP FUNCTION IF EXISTS log_event();
+
+DROP TABLE IF EXISTS logs;
+
+DROP TYPE IF EXISTS action;
+DROP TYPE IF EXISTS table_name;
+
 DROP TRIGGER IF EXISTS raise_error_on_num_modification ON sites;
 DROP TRIGGER IF EXISTS raise_error_on_num_modification ON sources;
 DROP TRIGGER IF EXISTS raise_error_on_num_modification ON images;
@@ -13,16 +25,21 @@ DROP FUNCTION IF EXISTS update_num_images();
 DROP TRIGGER IF EXISTS update_image_date_uploaded ON images;
 DROP FUNCTION IF EXISTS update_image_date_uploaded();
 
-DROP INDEX IF EXISTS images_source_id_idx;
-
 DROP TABLE IF EXISTS votes;
 DROP TABLE IF EXISTS users;
+
+DROP INDEX IF EXISTS images_source_id_idx;
+DROP INDEX IF EXISTS images_md5_idx;
+
 DROP TABLE IF EXISTS images;
+
+DROP INDEX IF EXISTS sources_state_idx;
+DROP INDEX IF EXISTS sources_last_refreshed_idx;
+
 DROP TABLE IF EXISTS sources;
+DROP TABLE IF EXISTS source_states;
 DROP TABLE IF EXISTS sites;
 
 DROP DOMAIN IF EXISTS media_file;
 DROP DOMAIN IF EXISTS url;
 DROP DOMAIN IF EXISTS uint;
-
--- TODO add DROPs for the other indexes and tables added recently
